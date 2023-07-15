af2bb767cad6e00af17515bf1eb664fe432be112
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-15 14:23:04 +0200

    todolist : init

---
+++ test-project-react/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // 
+import App from './App';
+
+// from boiler
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// old way get warning
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// React 18
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(<App />);
