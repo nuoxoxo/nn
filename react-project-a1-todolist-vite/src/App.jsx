9e39641a78e57e2a913d54baad9508c7cdd25bb7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-16 21:22:30 +0200

    todo on vite : jsx for list of jobs, del button

--- react-project-a1-todolist-vite/src/App.jsx
+++ react-project-a1-todolist-vite/src/App.jsx
@@ -4,14 +4,26 @@ export default function App() {
   return (  
     <>
       <form className='new-item-form'>
-        'Hello, World!'
         <div className='form-row new-item-form-row'>
-          <label htmlFor='item'>New Item</label>
+          {/* <label htmlFor='item'>New Item</label> */}
           <input type='text' id='item' />
           <button className='btn btn-add-item'>+ Job</button>
         </div>
       </form>
-      <h1 className='header'></h1>
+      <h1 className='header'>Jobs</h1>
+      <ul className='list'>
+
+        <li>
+          <label><input type='checkbox' /> job one </label>
+          <button className='btn btn-delete'> del job </button>
+        </li>
+
+        <li>
+          <label><input type='checkbox' /> job 2 </label>
+          <button className='btn btn-delete'> del job </button>
+        </li>
+
+      </ul>
     </>
   )
 }
