018883952bf0d3342d1455d3adc07809214c570d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-16 21:13:55 +0200

    todo on vite : return value wrapped in fragment (<></>)

--- react-project-a1-todolist-vite/src/App.jsx
+++ react-project-a1-todolist-vite/src/App.jsx
@@ -1,11 +1,17 @@
 import './styles.css'
 
 export default function App() {
-  return  <form className='new-item-form'>
-    'Hello, World!'
-    <div className='form-row new-item-form-row'>
-      <label htmlFor='item'>New Item</label>
-      <input type='text' id='item' />
-    </div>
-  </form>
+  return (  
+    <>
+      <form className='new-item-form'>
+        'Hello, World!'
+        <div className='form-row new-item-form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input type='text' id='item' />
+          <button className='btn btn-add-item'>+ Job</button>
+        </div>
+      </form>
+      <h1 className='header'></h1>
+    </>
+  )
 }
