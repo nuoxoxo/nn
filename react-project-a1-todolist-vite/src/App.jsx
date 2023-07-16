20c3c17fe3eaa781cfc77090ff8fd38a53594e2f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-16 21:09:58 +0200

    todo on vite : + css, now all black

--- react-project-a1-todolist-vite/src/App.jsx
+++ react-project-a1-todolist-vite/src/App.jsx
@@ -1,3 +1,11 @@
+import './styles.css'
+
 export default function App() {
-  return "Hello, World!"
+  return  <form className='new-item-form'>
+    'Hello, World!'
+    <div className='form-row new-item-form-row'>
+      <label htmlFor='item'>New Item</label>
+      <input type='text' id='item' />
+    </div>
+  </form>
 }
