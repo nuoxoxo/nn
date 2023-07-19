56db04e5d8bd6901e9f860ff83bb5ae86582ffb6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-19 21:46:06 +0200

    a3 : + tube

--- project-a3-vite-react-ts/src/App.tsx
+++ project-a3-vite-react-ts/src/App.tsx
@@ -1,8 +1,11 @@
+import Tube from './Tube'
+
 function App() {
   return (
-    <h1>
-      Hello, World!
-    </h1>
+    <>
+      <h1>Hello, World!</h1>
+      <div><Tube></Tube></div>
+    </>
   )
 }
 
---
+++ project-a3-vite-react-ts/src/Tube.tsx
@@ -0,0 +1,6 @@
+// PascalCasing: a react convention
+var Tube = () => {
+    return <h1>Here's Tube!!</h1>
+};
+
+export default Tube
