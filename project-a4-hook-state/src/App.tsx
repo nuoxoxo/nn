16801e1bf48e26275984a25a97661a04216ba375
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-22 11:53:10 +0200

    hook-state : works fine, now has 4 btns

--- project-a4-hook-state/src/App.tsx
+++ project-a4-hook-state/src/App.tsx
@@ -1,26 +1,37 @@
-import React from "react"
+import React from 'react'
+import { useState } from 'react'
+import GetRandomColorCSS from './GetRandomColorCSS'
 
 function App() {
 
-  let count: number = Math.floor(Math.random() * 2147483648)
-  let increment = () => { ++count, console.log('++', count) }
-  let decrement = () => { --count, console.log('--', count) }
+  const start = Math.floor(Math.random() * 2147483648)
 
-  console.log( 'App', count )
+  let [ count, // state variable
+        setCount // state changer func
+      ] = useState( start )
+
+  let increment = () => { setCount( ++count ) }
+  let decrement = () => { setCount( --count ) }
+  let random = () => { setCount( Math.floor(Math.random() * 2147483648) ) }
+  let reset = () => { setCount( 0 ) }
 
   return (
     <>
 
       <div>
+        <button onClick={ reset }>⏼</button>
+        <button onClick={ random }>⏻</button>
         <button onClick={ increment }>+</button>
         <button onClick={ decrement }>-</button>
       </div>
 
-      <div>{ count }</div>
+      <div style={ GetRandomColorCSS() }>{ count }</div>
 
       <div>
-        <button onClick={ decrement }>-</button>
+        <button onClick={ reset }>⏼</button>
+        <button onClick={ random }>⏻</button>
         <button onClick={ increment }>+</button>
+        <button onClick={ decrement }>-</button>
       </div>
 
     </>
