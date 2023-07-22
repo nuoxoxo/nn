16c0de94703203e4439d0a1f6218f7adf6a0885a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-22 12:34:12 +0200

    hook-state : done

--- project-a4-hook-state/src/App.tsx
+++ project-a4-hook-state/src/App.tsx
@@ -4,36 +4,49 @@ import GetRandomColorCSS from './GetRandomColorCSS'
 
 function App() {
 
-  const start = Math.floor(Math.random() * 2147483648)
+  const start = Math.floor(Math.random() * 32767)
+  const startCSS = GetRandomColorCSS()
 
-  let [ count, // state variable
-        setCount // state changer func
-      ] = useState( start )
+  let [ count, setCount ] = useState( start )
+  let [ css, setCSS ] = useState( startCSS )
 
   let increment = () => { setCount( ++count ) }
   let decrement = () => { setCount( --count ) }
-  let random = () => { setCount( Math.floor(Math.random() * 2147483648) ) }
   let reset = () => { setCount( 0 ) }
 
+  let random = () => {
+
+    setCount( Math.floor(Math.random() * 32767) )
+    setCSS( GetRandomColorCSS() )
+  }
+
+  interface GroupBTN {
+    Top: JSX.Element
+    Bottom: JSX.Element
+  }
+  
+  const group: GroupBTN = {
+    Top: (
+      <div>
+        <button onClick={reset}>⏼</button>
+        <button onClick={increment}>⍤</button>
+        
+      </div>
+    ),
+    Bottom: (
+      <div>
+        <button onClick={random}>⏻</button>
+        <button onClick={decrement}>⍥</button>
+      </div>
+    ),
+  }
+ 
   return (
+
     <>
-
-      <div>
-        <button onClick={ reset }>⏼</button>
-        <button onClick={ random }>⏻</button>
-        <button onClick={ increment }>+</button>
-        <button onClick={ decrement }>-</button>
-      </div>
-
-      <div style={ GetRandomColorCSS() }>{ count }</div>
-
-      <div>
-        <button onClick={ reset }>⏼</button>
-        <button onClick={ random }>⏻</button>
-        <button onClick={ increment }>+</button>
-        <button onClick={ decrement }>-</button>
-      </div>
-
+      { group.Top }
+      <div style={ css }>{ count }</div>
+      { group.Bottom }
     </>
   )
 }
