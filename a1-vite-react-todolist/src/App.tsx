bf6cf9da8050d34ca22b3a37dcd9ef9cbe8f068c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-23 20:15:10 +0200

    a1 todolist : bugs

--- a1-vite-react-todolist/src/App.tsx
+++ a1-vite-react-todolist/src/App.tsx
@@ -4,21 +4,55 @@ import './styles/styles.css'
 export default function App() {
 
   const [ newItem, setNewItem ] = useState('')
+  const [ todos, setTodos ] = useState([])
+
+  // const handleSubmit = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+    const handleSubmit = ( e ) => {
+
+    e.preventDefault()
+
+    setTodos( (currentTodos) => {
+      return [...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          checked: false
+        }
+      ]
+    })
+  }
 
   return (
 
     <>
-      <form className='new-item-form'>
+      <form className='new-item-form'
+        onSubmit={ handleSubmit }
+      >
         <div className='form-row new-item-form-row'>
           {/* <label htmlFor='item'>New Item</label> */}
-          <input type='text' id='item' />
+          <input type='text' id='item' 
+            value={ newItem }
+            onChange={ e => setNewItem( e.target.value )} />
           <button className='btn btn-add-item'>+ Job</button>
         </div>
       </form>
       <h1 className='header'>Jobs</h1>
       <ul className='list'>
 
-        <li>
+        {
+          todos.map( job => {
+            return
+            <li>
+              <label><input type='checkbox' checked={ job.checked } />
+                { job.title }
+              </label>
+              <button className='btn btn-delete'> del job </button>
+            </li>
+          })
+        }
+
+
+        {/* <li>
           <label><input type='checkbox' /> job one </label>
           <button className='btn btn-delete'> del job </button>
         </li>
@@ -26,7 +60,7 @@ export default function App() {
         <li>
           <label><input type='checkbox' /> job 2 </label>
           <button className='btn btn-delete'> del job </button>
-        </li>
+        </li> */}
 
       </ul>
     </>
