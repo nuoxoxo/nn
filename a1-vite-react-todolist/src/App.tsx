c20cab7fd679246f3a87ca2429ef1a2c7419a9de
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-24 23:42:52 +0200

    a1 todolist : get color from module

--- a1-vite-react-todolist/src/App.tsx
+++ a1-vite-react-todolist/src/App.tsx
@@ -1,41 +1,20 @@
 import { useState, useEffect } from 'react'
+import { getRandomColor, getOppositeColor } from './GetColor.tsx'
 
 const App = () => {
 
-  const [ bgc, setBGC ] = useState('')
-
-
-  const getRandomColor = (): string => {
-
-    const hex: string = '0123456789ABCDEF'
-    let res: string = '#'
-    let i: number = -1
-    while (++i < 6)
-      res += hex[Math.floor(Math.random() * 9)] // changed from 16 to favor more contrast
-    return res
-  }
-
-
-  const getOppositeColor = (color: string): string => {
-
-    color = color.slice(1) // pop the '#'
-    const rgb = parseInt(color, 16) // rgb <- hex
-    const white = 0xffffff
-    const invertedColor = '#' + (white - rgb).toString(16).padStart(6, '0');
-    return invertedColor
-  }
-
+  let [ bgc, setBGC ] = useState( getRandomColor() )
 
   useEffect( () => {
 
-    const c = getRandomColor()
-    const c_oppo = getOppositeColor(c)
+    bgc = getRandomColor()
+    const c_oppo = getOppositeColor(bgc)
 
-    setBGC(c)
+    setBGC(bgc)
 
-    document.body.style.backgroundColor = c
+    document.body.style.backgroundColor = bgc
     document.body.style.color = c_oppo
-  }, []) // strange syntax
+  }, []) // useEffect() : strange syntax
 
 
   return (
