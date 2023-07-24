d69f573da2a729ee2931c8ff4b5a65de420c3b59
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-24 15:15:01 +0200

    a1 todolist : redo init

--- a1-vite-react-todolist/src/App.tsx
+++ a1-vite-react-todolist/src/App.tsx
@@ -1,52 +1,13 @@
 import { useState } from 'react'
-import './styles/styles.css'
 
-export default function App() {
-
-  const [ newItem, setNewItem ] = useState('')
-  const [ todos, setTodos ] = useState([])
-
-  const handleSubmit = ( e: React.ChangeEvent<HTMLInputElement> ) => {
-
-    e.preventDefault()
-
-    setTodos( currentTodos => {
-      return [...currentTodos,
-        {
-          id: crypto.randomUUID(),
-          title: newItem,
-          checked: false
-        }
-      ]
-    })
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-
     <>
-      <form className='new-item-form'
-        onSubmit={ handleSubmit }
-      >
-        <div className='form-row new-item-form-row'>
-          {/* <label htmlFor='item'>New Item</label> */}
-          <input type='text' id='item' 
-            value={ newItem }
-            onChange={ e => setNewItem( e.target.value )} />
-          <button className='btn btn-add-item'>+ Job</button>
-        </div>
-      </form>
-      <h1 className='header'>Jobs</h1>
-      <ul className='list'>
-      todos.map( job => {
-            return
-            <li>
-              <label><input type='checkbox' checked={ job.checked } />
-                { job.title }
-              </label>
-              <button className='btn btn-delete'> del job </button>
-            </li>
-          })
-      </ul>
+      
     </>
   )
 }
+
+export default App
