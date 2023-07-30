a1b849e07b166f164d627d37dbe04e6a1c5ae5cb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-30 19:12:13 +0200

    runner test : dotenv + axios works

--- _sandbox/runner_dotenv_https.js
+++ _sandbox/runner_dotenv_https.js
@@ -1,50 +1,63 @@
-const https = require('https');
-const dotenv = require('dotenv');
+const https = require("https")
+const dotenv = require("dotenv")
 
-dotenv.config(); // Load environment variables from a .env file
+dotenv.config()
 
-const token = process.env.advent_of_code_session_token;
+const token = process.env.advent_of_code_session_token
 
 function get_input(day) {
-  const url = `https://adventofcode.com/2022/day/${day}/input`;
-  const headers = { Cookie: `session=${token}` };
+
+  const url = `https://adventofcode.com/2022/day/${day}/input`
+  const headers = { Cookie: `session=${token}` }
 
   return new Promise((resolve, reject) => {
+
     const req = https.get(url, { headers }, (resp) => {
-      let data = '';
+      let data = ""
 
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
+      resp.on("data", (chunk) => {
+        data += chunk
+      })
 
-      resp.on('end', () => {
-        console.log(url, '\n', resp.statusCode, '\n', resp.statusMessage, '\n', data);
+      resp.on("end", () => {
+        console.log(
+          url,
+          "\n",
+          resp.statusCode,
+          "\n",
+          resp.statusMessage,
+          "\n",
+          data
+        )
 
         if (resp.statusCode === 200) {
-          resolve(data);
+          resolve(data)
         } else {
-          reject(new Error(`/api/alerts response: ${resp.statusCode}: ${resp.statusMessage} \n${data}`));
+          reject(
+            new Error(
+              `response: ${resp.statusCode}: ${resp.statusMessage} \n${data}`
+            )
+          )
         }
-      });
-    });
+      })
+    })
 
-    req.on('error', (error) => {
-      reject(new Error(`Error while making the request: ${error.message}`));
-    });
+    req.on("error", (error) => {
+      reject(new Error(`err request: ${error.message}`))
+    })
 
-    req.end();
-  });
+    req.end()
+  })
 }
 
 async function main() {
   try {
-    const res = await get_input(10);
-    console.log(res[0], res.length);
+    const res = await get_input(10)
+    console.log(res[0], res.length)
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.error(error.message)
+    process.exit(1)
   }
 }
 
-main();
-
+main()
