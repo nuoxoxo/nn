525ca2c371970ec3646eb66664aad1d6e99195a9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-01 20:06:50 +0200

    a8 : 1st module returned in App

---
+++ a8-aoc/src/Aoc2210.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+
+const path = 
+  'https://raw.githubusercontent.com/nuoxoxo/simple-tasks/main/_inputs_/2210.0'
+
+var Aoc2210 = () => {
+  const [lines, setLines] = useState<string[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchLines = async () => {
+    try {
+      const resp = await fetch( path )
+      const temp = await resp.text()
+      const data: string[] = temp.trim().split('\n')
+      setLines(data)
+      setLoading(false)
+    } catch (error: any) {
+      console.error("Error fetching data: ", error);
+      setLoading(false);
+    }
+  }
+
+  //  Testing ...
+  // console.log(lines)
+
+  useEffect( () => {
+    fetchLines()
+  }, [])
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading data...</p>
+      ) : (
+        <pre>{lines ? lines.join('\n') : 'No data available.'}</pre>
+      )}
+    </div>
+  )
+}
+
+export default Aoc2210
--- a8-aoc/src/App.tsx
+++ a8-aoc/src/App.tsx
@@ -1,38 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import Aoc2210 from "./Aoc2210";
 
-const path = 'https://raw.githubusercontent.com/nuoxoxo/simple-tasks/main/_inputs_/2210.0'
-
-function App() {
-  const [lines, setLines] = useState<string[]>([])
-  const [loading, setLoading] = useState(true)
-
-  const fetchLines = async () => {
-    try {
-      const resp = await fetch( path )
-      const temp = await resp.text()
-      const data: string[] = temp.trim().split('\n')
-      setLines(data)
-      setLoading(false)
-    } catch (error: any) {
-      console.error("Error fetching data: ", error);
-      setLoading(false);
-    }
-  }
-
-  console.log(lines)
-
-  useEffect( () => {
-    fetchLines()
-  }, [])
+var App = () => {
 
   return (
-    <div>
-      {loading ? (
-        <p>Loading data...</p>
-      ) : (
-        <pre>{lines ? JSON.stringify(lines, null, 2) : 'No data available.'}</pre>
-      )}
-    </div>
+    <>
+      { <Aoc2210 /> }
+    </>
   )
 }
 
