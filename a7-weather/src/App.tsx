9a789758a120ea37d4cb9424405d5e11a070ac5c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-03 00:38:39 +0200

    weather man : current code not working

--- a7-weather/src/App.tsx
+++ a7-weather/src/App.tsx
@@ -1,13 +1,16 @@
-// import { useState } from 'react'
 import Researcher from './components/Researcher.ts'
 
 function App() {
 
+  const handleOnSearchChange = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <div>Hello, World!</div>
-      <div className='div-researcher'>
-        <Researcher />
+      <div className='container div-researcher'>
+        <Researcher onSearchChange={ handleOnSearchChange }/>
       </div>
     </>
   )
--- a7-weather/src/components/Researcher.ts
+++ a7-weather/src/components/Researcher.ts
@@ -1,6 +1,25 @@
-const Researcher = () => {
+import React, { useState } from 'react'
+import { AsyncPaginate, withAsyncPaginate } from 'react-select-async-paginate'
 
-  return ('Researcher!')
+const Researcher = ({ onSearchChange }: {onSearchChange: (data: any) => void }) => {
+
+  // return ('Researcher!')
+
+  const [search, setSearch] = useState( null )
+
+  const handleOnChange = ( data: any ) => {
+    setSearch(data)
+    onSearchChange(data)
+  }
+
+  return (
+    <AsyncPaginate
+      placeholder='City...'
+      debounceTimeout={ 600 }
+      value={ search }
+      onChange={ handleOnChange }
+    />
+  )
 }
 
 export default Researcher
