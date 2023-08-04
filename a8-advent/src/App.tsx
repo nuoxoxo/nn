c1b2dadc855003a4ef4bba20ffe09799631b2ebe
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 15:21:01 +0200

    a8 : new routes 1802

--- a8-advent/src/App.tsx
+++ a8-advent/src/App.tsx
@@ -1,5 +1,6 @@
 import Route2210 from "./Routes/Route2210"
 import Route2113 from "./Routes/Route2113"
+import Route1802 from "./Routes/Route1802"
 import { BrowserRouter as Router, Route, Routes /*, Link*/ } from "react-router-dom"
 
 var App = () => {
@@ -13,6 +14,10 @@ var App = () => {
               22:10</button>
             </li>
             <li>
+              <button onClick={() => window.location.href="/route1802"}>
+              18:02</button>
+            </li>
+            <li>
               <button onClick={() => window.location.href="/route2113"}>
               21:13</button>
             </li>
@@ -21,6 +26,7 @@ var App = () => {
 
         {/* must define routes below nav */}
         <Routes>
+          <Route path="/route1802" element={<Route1802 />} />
           <Route path="/route2210" element={<Route2210 />} />
           <Route path="/route2113" element={<Route2113 />} />
         </Routes>
