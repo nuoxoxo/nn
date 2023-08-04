63edee7c3eda6edea486714800cc157c80d81db4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 11:08:14 +0200

    a8 : solved p2

--- a8-advent/src/Route2210.tsx
+++ a8-advent/src/Route2210.tsx
@@ -9,13 +9,14 @@ var Route2210 = () => {
   const [lines, setLines] = useState<string[]>( [] )
   const [nums, setNums] = useState<number[]>( [] )
   const [p1, setP1] = useState<number>( 0 )
+  const [p2, setP2] = useState<string>( '' )
 
   const Solver_Part_One = () => {
 
-    let x: number = 1
     let tt: number = 0
+    let x: number = 1
     let i: number = 0
-    let cc: number = 0
+    let cycle: number = 0
     while (true) {
       if (nums.length === i) {
         i %= nums.length
@@ -26,12 +27,42 @@ var Route2210 = () => {
       }
       x += nums[i]
       i++
-      cc++
-      if (cc === 220) {
+      cycle++
+      if (cycle === 220) {
         break
       }
     }
     setP1(tt)
+  }
+
+  const Solver_Part_Two = () => {
+
+    let ss: string = ''
+    let x: number = 1
+    let i: number = 0
+    let cycle: number = 0
+    while (true) {
+      if (i === nums.length) {
+        i %= nums.length
+      }
+      let mod40: number = i % 40
+      if (mod40 === x - 1 || mod40 === x || mod40 === x + 1) {
+        ss += '@'
+      } else {
+        ss += ' '
+      }
+      if ((i + 1) % 40 === 0) {
+        ss += '\n'
+      }
+      x += nums[i]
+      cycle++
+      if (cycle === 240) {
+        break
+      }
+      console.log(cycle)
+      i++
+    }
+    setP2(ss)
   }
 
   const fetchLines = async () => {
@@ -65,20 +96,30 @@ var Route2210 = () => {
     Solver_Part_One()
   }, [nums])
 
+  useEffect(() => {
+    Solver_Part_Two()
+  }, [p2])
+
   return (
     <>
       {loading ? (
         <p>Loading data...</p>
       ) : (
-        <div className='container-L'>
-          <pre>
-            { lines ? lines.join('\n') : 'No data available.' }
-          </pre>
-          <pre>
-            { nums ? nums.join('\n') : 'No data available.' }
-          </pre>
-          <span>p1: { p1 }</span>
-        </div>
+        <>
+          <div className='container-L'>
+            <pre>
+              { lines ? lines.join('\n') : 'No data available.' }
+            </pre>
+            <pre>
+              { nums ? nums.join('\n') : 'No data available.' }
+            </pre>
+            <div className='container-R'>
+              <span>p1: { p1 }</span>
+              <span>p2: </span>
+              <span>{ p2 }</span>
+            </div>
+          </div>
+        </>
       )}
     </>
   )
