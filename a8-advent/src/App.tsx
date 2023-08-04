b6fa5f4d22237df7965fe41a4ef905e5338c411b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 15:04:20 +0200

    a8 : Done <--- navbar, 2 routes

--- a8-advent/src/App.tsx
+++ a8-advent/src/App.tsx
@@ -1,6 +1,6 @@
 import Route2210 from "./Routes/Route2210"
 import Route2113 from "./Routes/Route2113"
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes /*, Link*/ } from "react-router-dom"
 
 var App = () => {
   return (
@@ -9,15 +9,17 @@ var App = () => {
         <nav>
           <ul>
             <li>
-              <Link to="/route2210">y22d10</Link>
+              <button onClick={() => window.location.href="/route2210"}>
+              22:10</button>
             </li>
             <li>
-              <Link to="/route2113">y21d13</Link>
+              <button onClick={() => window.location.href="/route2113"}>
+              21:13</button>
             </li>
           </ul>
         </nav>
 
-        {/* define routes */}
+        {/* must define routes below nav */}
         <Routes>
           <Route path="/route2210" element={<Route2210 />} />
           <Route path="/route2113" element={<Route2113 />} />
