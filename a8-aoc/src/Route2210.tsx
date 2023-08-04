f43e0946ca153cde026ec1e9111d20c7db76da0f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 10:21:44 +0200

    restore lines : OK with CSS

--- a8-aoc/src/Route2210.tsx
+++ a8-aoc/src/Route2210.tsx
@@ -4,11 +4,14 @@ const path =
   'https://raw.githubusercontent.com/nuoxoxo/in/main/2210.0'
 
 var Route2210 = () => {
+
   const [loading, setLoading] = useState<boolean>( true )
+  const [lines, setLines] = useState<string[]>( [] )
   const [nums, setNums] = useState<number[]>( [] )
   const [res, setRes] = useState<number>( 0 )
 
   const Solver = () => {
+
     let x: number = 1
     let tt: number = 0
     let i: number = 0
@@ -33,18 +36,20 @@ var Route2210 = () => {
 
   const fetchLines = async () => {
     try {
+
       const resp = await fetch( path )
-      const temp = await resp.text()
+      const text = await resp.text()
+      const raws: string[] = text.trim().split('\n')
       const arr: number[] = []
-      const raw: string[] = temp.trim().split('\n')
-      for (let line of raw) {
+      for (let raw of raws) {
         arr.push(0)
-        let temp: string[] = line.split(' ')
+        let temp: string[] = raw.split(' ')
         if (temp.length === 2) {
           arr.push(parseInt(temp[1], 10))
         }
       }
       setNums(arr)
+      setLines(raws)
       setLoading(false)
     } catch (error: any) {
       console.error("Error fetching data: ", error);
@@ -65,12 +70,15 @@ var Route2210 = () => {
       {loading ? (
         <p>Loading data...</p>
       ) : (
-        <>
-          <span>res: { res }</span>
+        <div className='container'>
+          <pre>
+            { lines ? lines.join('\n') : 'No data available.' }
+          </pre>
           <pre>
             { nums ? nums.join('\n') : 'No data available.' }
           </pre>
-        </>
+          <span>res: { res }</span>
+        </div>
       )}
     </>
   )
