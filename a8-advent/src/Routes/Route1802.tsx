509230c5f6b273eb5005cc84fd18040ffe4ce457
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 15:43:33 +0200

    1802 : part 1 ok

--- a8-advent/src/Routes/Route1802.tsx
+++ a8-advent/src/Routes/Route1802.tsx
@@ -6,8 +6,40 @@ const path =
 
 var Route1802 = () => {
 
-  const [loading, setLoading] = useState<boolean>( true )
-  const [lines, setLines] = useState<string[]>( [] )
+  const [ loading, setLoading ] = useState<boolean>( true )
+  const [ lines, setLines ] = useState<string[]>( [] )
+  const [ p1, setP1 ] = useState<number>( 0 )
+
+  const Solver_Part_One = () => {
+
+    let c2: number = 0
+    let c3: number = 0
+    for (const line of lines) {
+      // console.log(line)
+      let D: number[] = new Array(26).fill(0)
+      for (const c of line) {
+        D[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
+      }
+      let ok2: boolean = false
+      let ok3: boolean = false
+      for (const d of D) {
+        if (d === 2) {
+          ok2 = true
+        }
+        if (d === 3) {
+          ok3 = true
+        }
+      }
+      if (ok2) {
+        c2++
+      }
+      if (ok3) {
+        c3++
+      }
+    }
+
+    setP1(c2 * c3)
+  }
 
   const handleData = async () => {
     try {
@@ -24,6 +56,10 @@ var Route1802 = () => {
     handleData()
   }, [])
 
+  useEffect( () => {
+    Solver_Part_One()
+  }, [lines])
+
   return (
     <>
       {loading ? (
@@ -33,6 +69,10 @@ var Route1802 = () => {
           <pre>
             { lines ? lines.join('\n') : 'No data available.' }
           </pre>
+          <div className='container-R'>
+            <span>--- 2018 Day 2: Inventory Management System ---</span>
+            <span>Part 1: { p1 }</span>
+          </div>
         </div>
       )}
     </>
