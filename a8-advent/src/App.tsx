b7cf1d0ca8bbf163961eb46be5c79a5fd9ec3f7c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-04 14:46:50 +0200

    a8 : + navbar with 2 routes

--- a8-advent/src/App.tsx
+++ a8-advent/src/App.tsx
@@ -1,13 +1,29 @@
 import Route2210 from "./Routes/Route2210"
 import Route2113 from "./Routes/Route2113"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 
 var App = () => {
-
   return (
-    <>
-      { <Route2210 /> }
-      { <Route2113 /> }
-    </>
+    <Router>
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/route2210">y22d10</Link>
+            </li>
+            <li>
+              <Link to="/route2113">y21d13</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* define routes */}
+        <Routes>
+          <Route path="/route2210" element={<Route2210 />} />
+          <Route path="/route2113" element={<Route2113 />} />
+        </Routes>
+      </>
+    </Router>
   )
 }
 
