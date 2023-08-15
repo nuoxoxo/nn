2ef2031c38d42cf82aa9f3a0562cbfa380d6b286
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-15 16:53:04 +0200

    bug : flex turns column w/o boots, weird

--- b8-markdown-previewer/src/App.tsx
+++ b8-markdown-previewer/src/App.tsx
@@ -1,85 +1,81 @@
-import { useState } from 'react'
-import { GetRandomColor } from './Helpers'
+import { useState } from "react";
+import { GetRandomColor } from "./Helpers";
 // import Badge from "react-bootstrap/Badge"
-import { marked } from "marked"
-
+import { marked } from "marked";
 
 function App() {
-  const [textColor, SetTextColor] = useState(GetRandomColor())
-  const [markdown, setMarkdown] = useState("")
+  const [textColor, SetTextColor] = useState(GetRandomColor());
+  const [markdown, setMarkdown] = useState("");
 
   let inputStyle = {
-
-    width: "60vh",
-    minHeight: "600px",
+    width: "500px",
+    height: "500px",
     backgroundColor: "#FEFBEA",
     marginLeft: "auto",
-    marginRight: "auto",
-    padding:"10px",
-    fontFamily: 'Georgia',
-    fontSize: '21px',
-  }
+    marginRight: "3px",
+    padding: "10px",
+    fontFamily: "Georgia",
+    fontSize: "21px",
+  };
 
   var outputStyle = {
-
-    width: "60vh",
-    minHeight: "600px",
+    width: "500px",
+    height: "500px",
     backgroundColor: "#E6FFE6",
-    marginLeft: "auto",
+    marginLeft: "3px",
     marginRight: "auto",
-    padding:"10px",
-    fontFamily: 'Georgia',
-    fontSize: '21px',
-  }
+    padding: "10px",
+    fontFamily: "Georgia",
+    fontSize: "21px",
+    textAligh: "left",
+  };
 
   return (
     <>
-
-      <div className='container'>
+      <div className="container">
         <span style={{ color: textColor }}>Hello</span>
         &nbsp;<span style={{ color: textColor }}>World</span>
       </div>
 
       <div className="container">
-
-          <div className="row mt-4">
-            <div className="col-md-6">
-              <div className="col text-center">
-                {/* <h4><Badge className="text-align-center" variant="secondary">Input</Badge></h4> */}
-                <div className="mark-input" >
-                <textarea 
-                    className="input" 
-                    style={ inputStyle }
-                    value={ markdown }
-                    onChange={ (e) => {
-                      setMarkdown(e.target.value)
-                    }}
-                  >
-                    {/* { console.log(markdown) } */}
-                  </textarea>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-6">
-              <div className="col text-center">
-              {/* <h4><Badge className="text-align-center" variant="secondary">Preview</Badge></h4> */}
-                <div className="mark-output" >
-                  <textarea 
-                    className="output" 
-                    style={ outputStyle }
-                    dangerouslySetInnerHTML={{ __html: marked(markdown) }}
-                  ></textarea>
-                </div>
+        <div className="row mt-4">
+          <div className="col-md-6">
+            <div className="col text-center">
+              {/* <h4><Badge className="text-align-center" variant="secondary">Input</Badge></h4> */}
+              <div className="mark-input">
+                <textarea
+                  className="input"
+                  style={inputStyle}
+                  value={markdown}
+                  onChange={(e) => {
+                    setMarkdown(e.target.value);
+                  }}
+                  placeHolder="👯 Write Your Markdown here 👯"
+                >
+                  {/* { console.log(markdown) } */}
+                </textarea>
               </div>
             </div>
           </div>
 
-          
-
+          <div className="col-md-6">
+            <div className="col">
+              {/* <h4><Badge className="text-align-center" variant="secondary">Preview</Badge></h4> */}
+              <div className="mark-output">
+                <div
+                  className="output"
+                  style={outputStyle}
+                  dangerouslySetInnerHTML={{
+                    __html: marked(markdown),
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
