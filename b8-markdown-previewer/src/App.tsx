20aa35abcbd2f7657d9480c2fa933329e3965748
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-15 17:17:17 +0200

    MDP : + Router for pre-deploy

--- b8-markdown-previewer/src/App.tsx
+++ b8-markdown-previewer/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
-import { GetRandomColor } from "./Helpers";
+import { useState } from "react"
+import { GetRandomColor } from "./Helpers"
 // import Badge from "react-bootstrap/Badge"
-import { marked } from "marked";
+import { marked } from "marked"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
-  const [textColor, SetTextColor] = useState(GetRandomColor());
-  const [markdown, setMarkdown] = useState("");
+  const [textColor, setTextColor] = useState(GetRandomColor())
+  const [markdown, setMarkdown] = useState("")
 
   let inputStyle = {
     width: "500px",
@@ -16,7 +18,7 @@ function App() {
     padding: "10px",
     fontFamily: "Georgia",
     fontSize: "21px",
-  };
+  }
 
   var outputStyle = {
     width: "500px",
@@ -28,54 +30,64 @@ function App() {
     fontFamily: "Georgia",
     fontSize: "21px",
     textAligh: "left",
-  };
+  }
 
   return (
-    <>
-      <div className="container">
-        <span style={{ color: textColor }}>Hello</span>
-        &nbsp;<span style={{ color: textColor }}>World</span>
-      </div>
-
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col-md-6">
-            <div className="col text-center">
-              {/* <h4><Badge className="text-align-center" variant="secondary">Input</Badge></h4> */}
-              <div className="mark-input">
-                <textarea
-                  className="input"
-                  style={inputStyle}
-                  value={markdown}
-                  onChange={(e) => {
-                    setMarkdown(e.target.value);
-                  }}
-                  placeHolder="👯 Write Your Markdown here 👯"
-                >
-                  {/* { console.log(markdown) } */}
-                </textarea>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/coughing/"}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="container">
+                <span style={{ color: textColor }}>Hello</span>
+                &nbsp<span style={{ color: textColor }}>World</span>
               </div>
-            </div>
-          </div>
 
-          <div className="col-md-6">
-            <div className="col">
-              {/* <h4><Badge className="text-align-center" variant="secondary">Preview</Badge></h4> */}
-              <div className="mark-output">
-                <div
-                  className="output"
-                  style={outputStyle}
-                  dangerouslySetInnerHTML={{
-                    __html: marked(markdown),
-                  }}
-                ></div>
+              <div className="container">
+                <div className="row mt-4">
+                  <div className="col-md-6">
+                    <div className="col text-center">
+                      {/* <h4><Badge className="text-align-center" variant="secondary">Input</Badge></h4> */}
+                      <div className="mark-input">
+                        <textarea
+                          className="input"
+                          style={inputStyle}
+                          value={markdown}
+                          onChange={(e) => {
+                            setMarkdown(e.target.value)
+                          }}
+                          placeholder="👯 Write Your Markdown here 👯"
+                        >
+                          {/* { console.log(markdown) } */}
+                        </textarea>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="col">
+                      {/* <h4><Badge className="text-align-center" variant="secondary">Preview</Badge></h4> */}
+                      <div className="mark-output">
+                        <div
+                          className="output"
+                          style={outputStyle}
+                          dangerouslySetInnerHTML={{
+                            __html: marked(markdown),
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+            </>
+          }
+        />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
