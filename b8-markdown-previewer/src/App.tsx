612acfed4c8265b83d6067db71dd38cc9081a197
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-08-15 19:42:47 +0200

    gig : fix Sass

--- b8-markdown-previewer/src/App.tsx
+++ b8-markdown-previewer/src/App.tsx
@@ -1,93 +1,40 @@
-import { useState } from "react"
-import { GetRandomColor } from "./Helpers"
+import { useState /*CSSProperties*/ } from "react";
+import { GetRandomColor } from "./Helpers";
 // import Badge from "react-bootstrap/Badge"
-import { marked } from "marked"
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { marked } from "marked";
 
 function App() {
-  const [textColor, setTextColor] = useState(GetRandomColor())
-  const [markdown, setMarkdown] = useState("")
-
-  let inputStyle = {
-    width: "500px",
-    height: "500px",
-    backgroundColor: "#FEFBEA",
-    marginLeft: "auto",
-    marginRight: "3px",
-    padding: "10px",
-    fontFamily: "Georgia",
-    fontSize: "21px",
-  }
-
-  var outputStyle = {
-    width: "500px",
-    height: "500px",
-    backgroundColor: "#E6FFE6",
-    marginLeft: "3px",
-    marginRight: "auto",
-    padding: "10px",
-    fontFamily: "Georgia",
-    fontSize: "21px",
-    textAligh: "left",
-  }
+  const [textColor] = useState(GetRandomColor());
+  const [markdown, setMarkdown] = useState("");
 
   return (
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/coughing/"}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="container">
-                <span style={{ color: textColor }}>Hello</span>
-                &nbsp<span style={{ color: textColor }}>World</span>
-              </div>
+    <>
+      <div className="container-hello" style={{ userSelect: 'none'}}>
+        <span style={{ color: textColor }}>Hello</span>
+        &nbsp;<span style={{ color: textColor }}>World</span>
+      </div>
 
-              <div className="container">
-                <div className="row mt-4">
-                  <div className="col-md-6">
-                    <div className="col text-center">
-                      {/* <h4><Badge className="text-align-center" variant="secondary">Input</Badge></h4> */}
-                      <div className="mark-input">
-                        <textarea
-                          className="input"
-                          style={inputStyle}
-                          value={markdown}
-                          onChange={(e) => {
-                            setMarkdown(e.target.value)
-                          }}
-                          placeholder="👯 Write Your Markdown here 👯"
-                        >
-                          {/* { console.log(markdown) } */}
-                        </textarea>
-                      </div>
-                    </div>
-                  </div>
+      <div className="mdp-body">
+        <div className="left">
+          <textarea
+            className="input"
+            value={markdown}
+            onChange={(e) => {
+              setMarkdown(e.target.value);
+            }}
+            placeholder="👯 Write Your Markdown here 👯"
+          ></textarea>
+        </div>
+        <div className="right">
+          <div
+            className="output"
+            dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+          ></div>
+        </div>
+      </div>
 
-                  <div className="col-md-6">
-                    <div className="col">
-                      {/* <h4><Badge className="text-align-center" variant="secondary">Preview</Badge></h4> */}
-                      <div className="mark-output">
-                        <div
-                          className="output"
-                          style={outputStyle}
-                          dangerouslySetInnerHTML={{
-                            __html: marked(markdown),
-                          }}
-                        ></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </>
-          }
-        />
-        <Route path="/about" element={<div>About</div>} />
-      </Routes>
-    </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- b8-markdown-previewer/src/main.tsx
+++ b8-markdown-previewer/src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
