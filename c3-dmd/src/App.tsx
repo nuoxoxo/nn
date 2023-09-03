f3500aeaa8f9a748345bdd14f9ae328e681a25d1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-03 19:11:06 +0200

    dmd : evaluate on change

--- c3-dmd/src/App.tsx
+++ c3-dmd/src/App.tsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/App.scss'
 import { GetRandomDateString } from "./helpers/Helpers";
 
 const App = () => {
   const [RandomDateArray/*, setRandomDateString*/] = 
     useState<[string, number, number]>(GetRandomDateString())
+  const [GuessVal, setGuessVal] = useState</*number | */string>('');
+  const [GuessRes, setGuessRes] = useState<string>('');
+
+  const EvaluateGuess = (val: number) => {
+    if (val === RandomDateArray[2]) {
+      setGuessRes('yes')
+    } else {
+      setGuessRes('no')
+    }
+  }
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value
+    console.log('set before evaluating: ', val)
+    setGuessVal(val)
+  }
+
+  useEffect(() => {
+    // EvaluateGuess(GuessVal) // bug
+    if (GuessVal) {
+      EvaluateGuess(parseInt(GuessVal))
+    }
+  }, [GuessVal])
+  
 
   return (
     <>
-      <h1>{ RandomDateArray.join(' ') }</h1>
+      <h1 className='QuestionDiv'>
+        { `${RandomDateArray[0]} ${RandomDateArray[1]}` }
+      </h1>
+      <div className='GuessingDiv'>
+        <label htmlFor='GuessBar'>Your Guess: </label>
+        <input id='GuessBar' type='number' onChange={handleOnChange}/>
+      </div>
+      <div>
+        { GuessRes }
+      </div>
     </>
   )
 }
