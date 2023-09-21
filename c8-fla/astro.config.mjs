5f558f0075f7b5cd2d9aaee0699eac9df1441b4d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-21 02:50:06 +0200

    fla : done

--- c8-fla/astro.config.mjs
+++ c8-fla/astro.config.mjs
@@ -1,4 +1,13 @@
+/*
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+*/
+
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://nuoxoxo.github.io',
+  base: '/fla',
+})
