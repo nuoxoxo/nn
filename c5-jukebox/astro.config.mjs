1154253086ca0f1e5769d4731a5e437561fdeacc
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-26 09:10:44 +0200

    c5 juke : update

--- c5-jukebox/astro.config.mjs
+++ c5-jukebox/astro.config.mjs
@@ -1,3 +1,14 @@
+/*
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
+
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+});
+*/
+
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
