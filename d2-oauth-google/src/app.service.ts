a4aeb405658dfa8c1d16d627b4d64976d56d2336
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-27 21:53:34 +0200

    app.serv : ggl login

--- d2-oauth-google/src/app.service.ts
+++ d2-oauth-google/src/app.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+
+  // getHello(): string {
+  //   return 'Hello World!';
+  // }
+
+  googleLogin (req) {
+    if ( ! req.user)
+      return 'no use(r)'
+    return {
+      messgage: 'user info from ggl',
+      user: req.user
+    }
+  } 
+
 }
