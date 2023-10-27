019f50d67117a095fafbd9ea4a30d068f8c78e20
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-27 21:53:15 +0200

    app.ctrl : useguard . AuthGuard

--- d2-oauth-google/src/app.controller.ts
+++ d2-oauth-google/src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,/*added:*/ UseGuards, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(  )
+  async googleAuth ( @Req() req) {
+
   }
+
+  @Get('auth/google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect( @Req() req ) {
+    return this.appService.googleLogin( req )
+  }
+
 }
