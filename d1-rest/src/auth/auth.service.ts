860b8f8d729fe60035e0ff787c6f773d8e845819
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-27 10:44:06 +0200

    minor

--- d1-rest/src/auth/auth.service.ts
+++ d1-rest/src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
-// import { User, Bookmark } from '@prisma/client';
 
 @Injectable (/*{}*/)
 export class AuthService {
