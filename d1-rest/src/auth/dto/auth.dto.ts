98c9e45ee2f8383cc0c41155e172e615fc0686c1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-27 11:13:03 +0200

    auth.dto : + class validator

--- d1-rest/src/auth/dto/auth.dto.ts
+++ d1-rest/src/auth/dto/auth.dto.ts
@@ -1,5 +1,13 @@
-export interface AuthDto {
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
-  email: string;
+// export interface AuthDto {
+export class AuthDto {
+
+  @IsEmail()
+  @IsNotEmpty()
+  mail: string;
+
+  @IsString()
+  @IsNotEmpty()
   pass: string
 }
