f7dccea5d7ba7bf09e151800bb6caf5990b3e0c0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-29 10:01:34 +0100

    d2 : get callback url from env

--- d2-oauth-google/src/google.strategy.ts
+++ d2-oauth-google/src/google.strategy.ts
@@ -14,7 +14,7 @@ export class GoogleStrategy extends PassportStrategy (
     super ({
       clientID : process.env.GOOGLE_CLIENT_ID,
       clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL : 'http://localhost:3333/auth/google/callback',
+      callbackURL : process.env.GOOGLE_CALLBACK_URL,
       scope : [ 'profile', 'email' ]
     });
   }
