c78af637eeba0599677fc0361ca36c4897ee4be0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-31 23:36:34 +0100

    d4 : no import in auth.module

--- d4-restful/src/auth/auth.module.ts
+++ d4-restful/src/auth/auth.module.ts
@@ -2,11 +2,11 @@ import { Module } from "@nestjs/common"; // autogen
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 // import { PrismaModule } from "src/prisma/prisma.module";
-// 👆 : no need once @Global in prisma.mod 
+// 👆 : no import once @Global in prisma.mod 
 
 @Module ({
 
-  // imports: [PrismaModule], // no need once @Global in prisma.mod 
+  // imports: [PrismaModule], // no import once @Global in prisma.mod 
   controllers: [AuthController],
   providers: [AuthService],
 
