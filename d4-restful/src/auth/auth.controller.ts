f7001fd94e9b8533061edee6f08e8e2a69e0d283
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-31 23:35:56 +0100

    d4 : auth.controller cleanup

--- d4-restful/src/auth/auth.controller.ts
+++ d4-restful/src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
 @Controller ('auth')
-// 'auth' is the "global prefix route" WTM.
+// 'auth' is the "global prefix route" 
 export class AuthController {
 
   constructor (private authService: AuthService)
@@ -13,49 +13,13 @@ export class AuthController {
   @Post('signup')
   signup (
     @Body () dto: AuthDto
-
-    // @Draft
-    // @Body('mail') mail:string,
-    // @Body('pass', ParseIntPipe /*validated an all-num pass*/) pass: string,
-
-    // @Draft
-    // @Body () dto: /*any*/AuthDto
-
-    // @Draft
-    // @Req() req: Request
   ) {
-    // @Draft
-    // console.log({mail, typeOfEmail: typeof mail, pass, typeOfPass: typeof pass})
-
-    // @DRaft
-    // console.log( dto ) // 1. raw
-    // console.log({ dto: dto }) // 2. same as 3.
-    // console.log({ dto, }) // 3. shorthand
-
-    // @Draft
-    // console.log(req.headers, req.body)
 
     return this.authService.signup()
   }
-  // @Draft
-  // { return { message: "@signup: Hello" } }
-  // { return 'Signed up.'}
 
-  // request : POST /auth/signin 
   @Post('signin')
-  signin ()
-  { return this.authService.signin() }
-  // @Draft
-  // { return { message: "@signin: Hello" } }
-  // { return 'Signed in.' }
-
-
-  // @Notes
-  // if no private keyword:
-  /*
-  authService: AuthService
-  constructor ( authService: AuthService) {
-    this.authService = authService
+  signin () {
+    return this.authService.signin()
   }
-  */
 }
