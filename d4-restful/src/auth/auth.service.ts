f36c5ca79c3bd046677b83a51b463eb41b5717d1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-01 16:38:37 +0100

    schema : throw credential repeats error

--- d4-restful/src/auth/auth.service.ts
+++ d4-restful/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable (/*{}*/)
 export class AuthService {
@@ -16,17 +17,31 @@ export class AuthService {
     const hash: string = await argon.hash( dto.pass )
 
     //  save new user to DB
-    const user = await this.prisma.user.create({
-      data: {
-        mail: dto.mail,
-        hash,
-      },
-    })
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          mail: dto.mail,
+          hash,
+        },
+      })
 
-    delete  user.hash
+      delete  user.hash
 
-    //  return saved user
-    return { user, message: "@signup service: Hello" }
+      //  return saved user
+      return { user, message: "@signup service: Hello" }
+    } catch ( err ) {
+
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002')
+        // error code P2002 represents `unique validation failure` (prisma)
+          throw new ForbiddenException(
+            'Credential taken', 
+            'Lol' // 2nd arg is "error": "Forbidden" by default
+          )
+          // ForbiddenException - NestJS built-in exception for 403
+      }
+      throw err
+    }
   }
 
   signin (){
