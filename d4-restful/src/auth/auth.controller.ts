302856fe5624f27423c1c36edb53bb670fc3b95f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-01 01:17:30 +0100

    d3 : auth.controller both (sign)methods use & prints the dto

--- d4-restful/src/auth/auth.controller.ts
+++ d4-restful/src/auth/auth.controller.ts
@@ -14,34 +14,40 @@ It sets the base path for all the routes defined within this controller
 */
 export class AuthController {
 
-  constructor (private authService: AuthService)
-  {}
+
+  constructor (private authService: AuthService) {}
   /*
   Constructor takes an instance of AuthService class as a param/
   The AuthService class should have been decorated as `Injectable`
   */
 
-  // request : POST to /auth/signup
+
+  /*
+  ` @Post('signup') `
+  marks the signup method to handle HTTP POST requests to the '/auth' route.
+  It specifies the '/auth/signup' path for this specific route
+  */
   @Post('signup')
   signup (
-    @Body () dto: AuthDto
+    @Body()
+    dto: AuthDto
     /*
     ` @Body() `
     a decorator that instructs NestJS to parse the request body 
     and bind it to the parameter 'dto' of type 'AuthDto'
     */
   ) {
-
+    console.log("from signup ::", dto)
     return this.authService.signup()
   }
-  /*
-  ` @Post('signup') `
-  marks the signup method to handle HTTP POST requests to the '/auth' route.
-  It specifies the '/auth/signup' path for this specific route
-  */
+
 
   @Post('signin')
-  signin () {
+  signin (
+    @Body()
+    dto: AuthDto
+  ) {
+    console.log("from signin ::", dto)
     return this.authService.signin()
   }
 }
