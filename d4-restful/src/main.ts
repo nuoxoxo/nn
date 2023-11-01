46325c41e057e3a6f68653fecdeba273615d8e6d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-01 01:37:04 +0100

    d4 : main --> + app.useGlobalPipes( new ValidationPipe () )

--- d4-restful/src/main.ts
+++ d4-restful/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   /*
@@ -8,12 +9,14 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes( new ValidationPipe () )
+
   await app.listen(10086);
   /*
   start listening for incoming HTTP requests on port 10086
   */
 
-  console.log(AppModule)
+  console.log(AppModule, typeof AppModule)
 
 }
 
