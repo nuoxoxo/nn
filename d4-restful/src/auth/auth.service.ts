a8982648888d64c08cb9d83f9b3cb68f91ea0845
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-03 00:47:14 +0100

    d4 : jwt in auth.module & auth.service

--- d4-restful/src/auth/auth.service.ts
+++ d4-restful/src/auth/auth.service.ts
@@ -3,11 +3,15 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable (/*{}*/)
 export class AuthService {
 
-  constructor (private prisma : PrismaService) {}
+  constructor (
+    private prisma : PrismaService,
+    private jwt : JwtService,
+  ) {}
 
   async signup (dto: AuthDto) {
     // async : because Prisma is called asynchronously
