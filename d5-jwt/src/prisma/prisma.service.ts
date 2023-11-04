e5f905f8d3d6ff72e8c569277374374cd8c8e1bb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-04 22:32:18 +0100

    d5 : added env vars . db inside prisma.service

--- d5-jwt/src/prisma/prisma.service.ts
+++ d5-jwt/src/prisma/prisma.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as dotenv from 'dotenv'
+
 
 @Injectable()
 // export class PrismaService {} // raw
@@ -7,7 +9,14 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
     constructor () {
-      super ()
+      dotenv.config()
+      super ({
+        datasources: {
+          db: {
+            url: process.env.DATABASE_URL
+          }
+        }
+      })
     }
     onModuleDestroy() {}
     onModuleInit() {}
