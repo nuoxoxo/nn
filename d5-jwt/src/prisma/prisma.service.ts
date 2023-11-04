04a2b40e4395930f17b44b1a80a4d63c226fc742
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-04 22:22:26 +0100

    d5 : + constructor inside prisma.service

--- d5-jwt/src/prisma/prisma.service.ts
+++ d5-jwt/src/prisma/prisma.service.ts
@@ -2,4 +2,13 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+// export class PrismaService {} // raw
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+    constructor () {
+      super ()
+    }
+    onModuleDestroy() {}
+    onModuleInit() {}
+}
