cff645c5bf13f44b758197bfcb5e6db8088ba9d7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-05 17:25:54 +0100

    d5 : make pipes global in main . bugfix in auth.serv

--- d5-jwt/src/auth/auth.service.ts
+++ d5-jwt/src/auth/auth.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
 
   constructor ( private prisma: PrismaService ) {}
 
-  local_signup(dto: AuthService) {
+  local_signup(dto: AuthDto) {
     const new_user = this.prisma.user.create({
       data: {
-        mail: dto.mail
+        mail: dto.mail,
+        pass
       }
     })
   }
