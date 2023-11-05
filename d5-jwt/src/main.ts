7ae6a58f7061eb3977970ebe0a2e13fcade598c5
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-05 19:57:41 +0100

    d4 : bugfix in schema - user_s_

--- d5-jwt/src/main.ts
+++ d5-jwt/src/main.ts
@@ -6,7 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 dotenv.config()
 
 async function bootstrap() {
-  console.log(process.env)
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe)
   await app.listen(process.env.LISTEN_PORT)
