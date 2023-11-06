3439f46bb83d337646d51e3fe88738890f146e0c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-06 12:36:44 +0100

    auth.ctrl : + httpcode(httpstatus)

--- d5-jwt/src/auth/auth.controller.ts
+++ d5-jwt/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Token } from './types';
@@ -17,19 +17,23 @@ export class AuthController {
   constructor ( private authService: AuthService ) {}
 
   @Post('/local/signup')
+  @HttpCode (HttpStatus.CREATED) // 201
   async local_signup (@Body() dto: AuthDto) : Promise<Token> {
     return this.authService.local_signup(dto)
   }
 
   @Post('/local/signin')
+  @HttpCode (HttpStatus.OK) // 200
   async local_signin (@Body() dto: AuthDto) : Promise<Token> {
     return this.authService.local_signin(dto)
   }
 
   @Post('/refresh')
+  @HttpCode (HttpStatus.OK) // 200
   refresh () { return this.authService.refresh() }
 
   @Post('/logout')
+  @HttpCode (HttpStatus.OK) // 200
   logout (uid: number) : Promise<void> {
     return this.authService.logout( uid )
   }
