38f82e8b9f157af9140c85d505629840aa196c54
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-06 12:08:55 +0100

    d5 : no dotenv import in prisma service

--- d5-jwt/src/prisma/prisma.service.ts
+++ d5-jwt/src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import * as dotenv from 'dotenv'
+// import * as dotenv from 'dotenv'
 
 
 @Injectable()
@@ -9,7 +9,7 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
     constructor () {
-      dotenv.config()
+      // dotenv.config()
       super ({
         datasources: {
           db: {
