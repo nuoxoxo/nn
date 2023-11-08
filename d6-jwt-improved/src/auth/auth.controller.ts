11d8cdba3e99a4b5e0b00ec18965467e0d1ee3b0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-08 01:00:20 +0100

    d6 : ATGuard is now global . bypass ATGuard for some routes

--- d6-jwt-improved/src/auth/auth.controller.ts
+++ d6-jwt-improved/src/auth/auth.controller.ts
@@ -2,10 +2,14 @@ import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@n
 import { AuthService } from './auth.service';
 import { AuthDto, /*let's try this*//*RefreshTokenDto*/ } from './dto';
 import { Token } from './types';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express'
-import { ATGuard, RTGuard } from 'src/common/guards';
-import { get_current_user_id, get_current_user } from 'src/common/decorators';
+// import { AuthGuard } from '@nestjs/passport';
+// import { Request } from 'express'
+import {/*ATGuard,*/ RTGuard} from 'src/common/guards';
+import {
+  Get_current_user_id, 
+  Get_current_user, 
+  Public
+} from 'src/common/decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -20,18 +24,21 @@ export class AuthController {
 
   constructor ( private authService: AuthService ) {}
 
+  @Public()
   @Post('/local/signup')
   @HttpCode (HttpStatus.CREATED) // 201
   async local_signup (@Body() dto: AuthDto) : Promise<Token> {
     return this.authService.local_signup(dto)
   }
 
+  @Public()
   @Post('/local/signin')
   @HttpCode (HttpStatus.OK) // 200
   async local_signin (@Body() dto: AuthDto) : Promise<Token> {
     return this.authService.local_signin(dto)
   }
 
+  @Public() // improved : bypass the now global ATGuard
   // guard way 1
   // @UseGuards(AuthGuard('Jwt-Refresh'))
   // guard way 2 : Improved w/ custom Guard
@@ -42,8 +49,8 @@ export class AuthController {
   // refresh (@Req() req: Request) {
   // Decorator way 2 : 
   refresh (
-    @get_current_user_id() uid: number,
-    @get_current_user('refresh_token') rtk: string,
+    @Get_current_user_id() uid: number,
+    @Get_current_user('refresh_token') rtk: string,
   ) {
 
     // way 2 : Improved w/ custom Decorator
@@ -57,13 +64,14 @@ export class AuthController {
   // guard way 1
   // @UseGuards(AuthGuard('Jwt'))
   // guard way 2 : Improved w/ custom Guard 
-  @UseGuards( ATGuard )
+  // @UseGuards( ATGuard )
+  // guard way 3 : use global Guard
   @Post('/logout')
   @HttpCode (HttpStatus.OK) // 200
   // Decorator way 1 : param uses express request
   // logout (@Req() req: Request) : Promise<boolean> {
   // Decorator way 2 : Improved w/ custom decorator
-  logout (@get_current_user_id() uid: number) : Promise<boolean> {
+  logout (@Get_current_user_id() uid: number) : Promise<boolean> {
     console.log("auth/logout @ controller :", {uid})
     // const uid = req.user['sub'] // cmt out w/ custom decorator
     return this.authService.logout( uid )
--- d6-jwt-improved/src/common/decorators/decorators.decorator.ts
+++ d6-jwt-improved/src/common/decorators/decorators.decorator.ts
@@ -1,7 +1,8 @@
-import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { ExecutionContext, SetMetadata, createParamDecorator } from "@nestjs/common";
 
-export const get_current_user_id = createParamDecorator (
+export const Get_current_user_id = createParamDecorator (
   (field: undefined, context: ExecutionContext) : number => {
+
     const request = context.switchToHttp().getRequest()
     return request.user['sub']
     /*
@@ -12,11 +13,17 @@ export const get_current_user_id = createParamDecorator (
   }
 )
 
-export const get_current_user = createParamDecorator (
+export const Get_current_user = createParamDecorator (
   (field: string | undefined | undefined, context: ExecutionContext) : number => {
+
     const request = context.switchToHttp().getRequest()
     if (!field)
       return request.user
     return request.user[field]
   }
 )
+
+export const Public = () => SetMetadata("isPublic", true)
+// export const Public = () => {
+//   return SetMetadata("isPublic", true)
+// }
