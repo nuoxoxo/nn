9c43b306dce2c5830ed8dca52a0b9557600005c2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-11 22:27:25 +0100

    d4 : myself route done

--- d4-restful/src/auth/strategy/jwt.strategy.ts
+++ d4-restful/src/auth/strategy/jwt.strategy.ts
@@ -1,13 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy (
   Strategy,
   'Jwt', // will be identified by the AuthGuard 
 ) {
-  constructor() {
+  constructor(
+    private prisma: PrismaService
+  ) {
     super({
       jwtFromRequest:
         ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -17,8 +20,21 @@ export class JwtStrategy extends PassportStrategy (
 
   // TODO : validate func should be handmade
 
-  validate(payload: any) {
-    console.log(payload, 'from jwt.strat.validate')
+  // validate(payload: any) {
+  async validate(
+    payload: {
+      sub: number,
+      mail: string
+    }
+  ) {
+    console.log('jwt.strat.validate ::')
+    console.log({payload})
+    console.log('whatever passed here will be set as value of the user key of the request object (at user.ctrl)\n')
+
+    const user = await this.prisma.user.findUnique({where: {id: payload.sub}})
+
+    delete user.hash
+    return user
   }
 
 }
--- d4-restful/src/user/user.controller.ts
+++ d4-restful/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
@@ -9,24 +9,27 @@ export class UserController {
   @Get()
   */
 
-  // GET /users/me . GUARDED
   @UseGuards ( // route will be guarded by this strategy set
     AuthGuard('Jwt'),
   )
   @Get('myself')
-  get_me () {
-    return 'I am the center'
+  get_me (@Req() req: Request) {
+    console.log('user.ctrl.Get/myself ::')
+    console.log({user: req['user']})
+    return req['user']
+    // return req.user // doesn't work don't know why
+    // return 'I am the center'
   }
 
-  // GET /users/myself . UnGuarded
+  // UnGuarded
   @Get('me')
   get_myself () {
-    return 'i me mine'
+    return "i am mine<br><img src='https://upload.wikimedia.org/wikipedia/commons/2/2d/Dados_4_a_20_caras_trans.png'>"
   }
 
-  // GET /users/mine . UnGuarded
+  // UnGuarded
   @Get('mine')
   get_mine () {
-    return 'i am mine'
+    return "i am mine"
   }
 }
