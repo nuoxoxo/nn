4d4efca9a3e9deff68d71d0f528a973b653c7fc2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-11 21:14:18 +0100

    minor

--- e0-sqlize-node/index.js
+++ e0-sqlize-node/index.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {res.send('<br /><img src="https://static.npmjs.com/a
 app.get('/world', (req, res) => {res.send("<h1>Hello!</h1><br /><img src='https://avatars.githubusercontent.com/u/6078720?s=200&v=4'>")})
 app.get('/hello', (req, res) => {res.send("<h1>World!</h1><br /><img src='https://avatars.githubusercontent.com/u/22247014?s=200&v=4'>")})
 app.get('/42', (req, res) => {res.send("<img src='https://raw.githubusercontent.com/sequelize/sequelize/HEAD/logo.svg'>")})
+app.get('/21', (req, res) => {res.send("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Dados_4_a_20_caras_trans.png/225px-Dados_4_a_20_caras_trans.png'>")})
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
