2bd5e18cc97dac8d37c466e5e0281ba722311f83
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-13 16:31:16 +0100

    d9 : upd. module nestjs/testing

--- d9-restful/test/app.e2e-spec.ts
+++ d9-restful/test/app.e2e-spec.ts
@@ -1,5 +1,10 @@
 import { describe } from "node:test";
+import { TEST } from '@nestjs/testing'
 
 describe('app e2e', () => {
+
+  beforeAll(async () => {
+    const module_ref = await 
+  })
   it.todo('hello test')
 })
