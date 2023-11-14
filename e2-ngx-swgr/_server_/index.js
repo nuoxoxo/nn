259d3a6f26cd70973690875948b3125d14f2feca
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-14 21:25:54 +0100

    e2 : requrie

--- e2-ngx-swgr/_server_/index.js
+++ e2-ngx-swgr/_server_/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+
 require('dotenv').config()
 const port = process.env.E_ONE_PORT
+
+const swaggerDOC = require('swagger-jsdoc')
+// const swaggerUI = require('swagger-ui-express')
 
 app.get('/', (req, res) => {
     res.send('<h1>welcome to one of the many enpoints</h1>')
