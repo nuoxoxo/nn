a1409be3dd9713fd45400f113eca8b36cbf3617d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-14 21:32:31 +0100

    stuck

--- e2-ngx-swgr/_server_/index.js
+++ e2-ngx-swgr/_server_/index.js
@@ -5,7 +5,22 @@ require('dotenv').config()
 const port = process.env.E_ONE_PORT
 
 const swaggerDOC = require('swagger-jsdoc')
-// const swaggerUI = require('swagger-ui-express')
+const swaggerUI = require('swagger-ui-express')
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        servers: [{
+            url: 'http://localhost:' + port,
+        }]
+    },
+    apis: []
+}
+const spacs = swaggerDOC(options)
+app.use(
+    '/api-docs',
+    swaggerUI.serve,
+    swaggerUI.setup(spacs)
+)
 
 app.get('/', (req, res) => {
     res.send('<h1>welcome to one of the many enpoints</h1>')
