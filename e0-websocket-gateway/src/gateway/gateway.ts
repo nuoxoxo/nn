bc994e0e797a0676714dc9d91d9f5ad1484e5180
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-17 21:55:50 +0100

    eO : listen to disConn inside server.on(conn) block

--- e0-websocket-gateway/src/gateway/gateway.ts
+++ e0-websocket-gateway/src/gateway/gateway.ts
@@ -1,9 +1,38 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { OnModuleInit } from "@nestjs/common";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from "socket.io";
 
 @WebSocketGateway()
-export class myGateway {
-  @SubscribeMessage('newMessagePattern') // param is "chosen pattern to be fulfilled"
+export class myGateway implements OnModuleInit {
+
+  @WebSocketServer()
+  server: Server;
+
+  private connCount: number = 0
+  private disconnCount: number = 0
+
+  onModuleInit() {
+    this.server.on(
+      'connection',
+      (sock) => {
+        console.log(sock.id, 'connected', this.connCount++)
+        sock.on('disconnect', () => {
+          console.log(sock.id, 'disconnected', this.disconnCount++)
+        })
+      }
+    )
+  }
+
+  onModuleDestroy(signal: string) {
+    console.log(signal, 'connected', this.disconnCount++)
+  }
+
+  @SubscribeMessage('route_one') // param: a pattern to be fulfilled
   onNewMsg (@MessageBody () payload: any) {
     console.log( payload )
+    this.server.emit(
+      'route_two',
+      `hello! (replying to \"${payload}\")`
+    )
   }
 }
