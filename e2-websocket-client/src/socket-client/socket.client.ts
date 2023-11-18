bafd65d6c7d670a9d289e63b2124fa8576ca4cb5
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-18 09:41:59 +0100

    socket.io client : sock cli constructor

--- e2-websocket-client/src/socket-client/socket.client.ts
+++ e2-websocket-client/src/socket-client/socket.client.ts
@@ -1,4 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { Socket, io } from "socket.io-client";
 
 @Injectable()
-export class SocketClient {}
+export class SocketClient {
+
+  public sockCli: Socket
+
+  constructor () {
+    this.sockCli = io('http://localhost:10086')
+  }
+
+}
