6acb98232ece8add1a16434b0998657ce3c6ea1c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-18 10:30:12 +0100

    e0 : bugfix . to try on postman .

--- e0-websocket-gateway/src/gateway/gateway.ts
+++ e0-websocket-gateway/src/gateway/gateway.ts
@@ -17,11 +17,11 @@ export class myGateway implements OnModuleInit {
   // private disconnCount: number = 0
 
   onModuleInit() {
-    this.server.on( // StrictEventEmitter.on<ev>(ev: 'conn', listener: (sock) => void)
+    this.server.on( // StrictEventEmitter.on<ev>(ev: 'conn', listener: (sock) => void) /// proto
       'connect', // @param ev: "connection|connect"
       (sock) => { // @param listener: callback func
         console.log('Server', sock.id, 'connected', `(${this.connCount++})`)
-        sock.on( // StrictEventEmitter.on<ev>(ev: 'conn', listener: (sock, dscp?) => void)
+        sock.on( // StrictEventEmitter.on<ev>(ev: 'conn', listener: (sock, dscp?) => void) /// proto
           'disconnect', // @param ev: "disconnect"
           (reason, dscp) => { // @param listener: callback func
             console.log(
@@ -32,6 +32,18 @@ export class myGateway implements OnModuleInit {
         )
       }
     )
+    // This way to listen to 'disconn' won't work. Correct way above
+    /*
+    this.server.on(
+      'disconnect',
+      (reason, dscp) => {
+        console.log(
+          'Server', 'disconnected', 
+          `(reason: ${reason}, dscp: ${dscp})`
+        )
+      }
+    )
+    */
   }
 
   onModuleDestroy(signal: string) {
