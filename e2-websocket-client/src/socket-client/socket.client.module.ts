dea75c6b1edf991c508a788a52e62917b180ea3f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-18 09:31:29 +0100

    socket.io client : socket.mod + socket.cli

--- e2-websocket-client/src/socket-client/socket.client.module.ts
+++ e2-websocket-client/src/socket-client/socket.client.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { SocketClient } from "./socket.client";
 
 @Module({
-  providers: [],
+  providers: [SocketClient],
 })
 
 export class SocketModule {}
