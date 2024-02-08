00af1903cd51d19a4501abb44299890f83ded231
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-08 11:30:47 +0100

    e5/first skecth works fine

--- e5-p5react-using-react-p5/src/App.jsx
+++ e5-p5react-using-react-p5/src/App.jsx
@@ -1,13 +1,13 @@
-// import { useState } from 'react'
-
 import './App.scss'
-
+import Rotate_oop from './p5components/rotate_oop'
+// import { useState } from 'react'
 
 function App() {
 
   return (
     <>
       <h1>hello, world</h1>
+      <Rotate_oop />
     </>
   )
 }
