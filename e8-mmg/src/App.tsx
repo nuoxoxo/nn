4b9a20d64c5e5d247b8bdc62643dca090790b880
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-20 23:35:24 +0100

    e8/we have our cards, shuffled. todo/render cards based on url

--- e8-mmg/src/App.tsx
+++ e8-mmg/src/App.tsx
@@ -1,12 +1,60 @@
 // import { useState } from 'react'
+import { useState } from 'react'
 import './App.scss'
 
+const makeImgurStr = (id: string): string => {
+  return 'https://i.imgur.com/' + id + '.jpeg'
+}
+
+const CardsImg: { src: string }[] = [
+  { 'src': 'Hz1X0JH' },
+  { 'src': '9yv2mCJ' },
+  { 'src': '6MG5HPe' },
+  { 'src': 'zFmGDB4' },
+  { 'src': 'fkCw83t' },
+  { 'src': 'Aj8rD5P' },
+  { 'src': 'QP32pW4' },
+  { 'src': 'S6XtdpH' },
+  // { 'src': 'N90grdZ' },
+  // { 'src': 'bV9BjHm' },
+  // { 'src': 'lsnQcbr' },
+  // { 'src': 'OwDfLkA' },
+  // { 'src': 'jWy7uCZ' },
+  // { 'src': 'YjjHHWq' },
+  // { 'src': 'BsP0ADp' },
+  // { 'src': 'WYnL0R8' },
+]
+
 const App = () => {
+
+  const [ Turns, setTurns ] = useState(0)
+  const [ Cards, setCards ] = useState<{
+    src: string;
+    id: number;
+    url: string
+  }[]>([])
+
+  const shuffleCards = () => {
+    const res = [...CardsImg, ...CardsImg]
+      .sort(() => Math.random() - .5)
+      .map( (card) => ({
+        ...card, 
+        id: Math.random(),
+        url: makeImgurStr(card.src)
+      }))
+    setCards( res )
+    setTurns( 0 )
+  }
+
+  console.log('/test shuffle', Turns, Cards)
+
   return (
     <>
       <div className='App'>
         <h1> hello, world!</h1>
-        <button>New Game</button>
+        <button
+          onClick={ shuffleCards }
+        >New Game</button>
       </div>
     </>
   )
