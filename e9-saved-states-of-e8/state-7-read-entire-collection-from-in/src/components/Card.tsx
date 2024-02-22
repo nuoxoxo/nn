e63243c1cfa1029125fd3d3a47340e8d41a0ff9d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-22 01:17:50 +0100

    state 7/entire collection read with success

---
+++ e9-saved-states-of-e8/state-7-read-entire-collection-from-in/src/components/Card.tsx
@@ -0,0 +1,32 @@
+import './Card.scss'
+
+const Card = ({ c, flipped, handleGuessing, CardBackDefault, DuringFlip, } : {
+  c: { src: string; id: number; url: string; matched: boolean}
+  handleGuessing: (c: {src: string; id: number; url: string; matched: boolean}) => void
+  flipped: boolean
+  DuringFlip: boolean
+  CardBackDefault: string
+}) =>  {
+
+  const handleClick = () => {
+    if ( DuringFlip ) return
+    console.log(c.url)
+    handleGuessing( c )
+  }
+
+  return (
+    <div key={c.id} className='cards-card-div'>
+      <div className={flipped ? 'flipped' : ''}>
+        {/* <img className='cards-card cards=card-front' src={ c.url } /> */}
+        <img className='cards-card cards-card-back' src={ c.url }
+          // onClick={ handleClick }
+        />
+        <img className='cards-card cards-card-front' src={CardBackDefault}
+          onClick={ handleClick }
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Card
