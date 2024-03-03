4b00b9926ba7e28eb9f789802f0631366d7a8dc9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-03 18:02:52 +0100

    g2/import react router dom

--- g2-bilancio/src/App.tsx
+++ g2-bilancio/src/App.tsx
@@ -1,11 +1,33 @@
 // import { useState } from 'react'
 import './App.scss'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
 
 function App() {
+
+  // about react-router-dom
+  // https://reactrouter.com/en/main/start/tutorial
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <h1>hello, world!</h1>,
+    },
+    {
+      path: "/about",
+      element: <h1>About</h1>,
+    },
+  ]);
+
   return (
-    <div className='App'>
-      hello, world!
-    </div>
+    <>
+      <div className='App'>
+        hello, world!
+        <RouterProvider router={router} />
+      </div>
+    </>
   )
 }
 
