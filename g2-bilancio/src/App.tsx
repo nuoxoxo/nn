f3e7d2cefd3cfe4fec2bc7b5bfb28363641cf10b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-03 19:38:16 +0100

    g2/ + Dashboard

--- g2-bilancio/src/App.tsx
+++ g2-bilancio/src/App.tsx
@@ -4,21 +4,22 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom"
-
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
 
   // about react-router-dom
   // https://reactrouter.com/en/main/start/tutorial
-  const routes = []
+  const routes: { path: string; element: JSX.Element }[] = []
   for (let i = 0; i < 1001; i++) {
     routes.push({path: '/' + i, element: <h1>{i}</h1>})
   }
   const router = createBrowserRouter([
     ... routes,
+    // To fix : 404 on undefined routes
     {
       path: "/",
-      element: <h1>hello, world!</h1>,
+      element: <Dashboard />,
     },
     {
       path: "/about",
